8917ae7305ebb159f7c0ce065f63c0288b756b88
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-19 22:53:32 +0300

    Initial release

---
+++ frappe_login_session_defaults_popup/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Frappe Login Session Defaults Popup",
+            "color": "blue",
+            "icon": "octicon octicon-plus",
+            "type": "module",
+            "label": _("Frappe Login Session Defaults Popup")
+        }
+    ]
