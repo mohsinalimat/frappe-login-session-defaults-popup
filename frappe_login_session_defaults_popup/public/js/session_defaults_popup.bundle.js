d39e548c09422e24a0753fb92da085f790ae7b0b
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-20 16:04:48 +0300

    Update to v1.0.1

--- frappe_login_session_defaults_popup/public/js/session_defaults_popup.bundle.js
+++ frappe_login_session_defaults_popup/public/js/session_defaults_popup.bundle.js
@@ -1,7 +1,7 @@
 async function _initLoginSessionDefaultsPopup() {
     let res = await frappe.call({
         type: 'GET',
-        method: 'frappe_login_session_defaults_popup.api.session.get_settings',
+        method: 'frappe_login_session_defaults_popup.override.get_settings',
     });
     let data = await res.message;
     await frappe.require([
@@ -9,7 +9,7 @@ async function _initLoginSessionDefaultsPopup() {
         'assets/frappe/js/frappe/socketio_client.js',
     ], () => {
         frappe.socketio.init(data.socketio_port);
-        frappe.realtime.on('login_session_created', function(res) {
+        frappe.realtime.on('login_event_triggered', function(res) {
             frappe.ui.toolbar.setup_session_defaults();
         });
     });
