33305ee7f317e4eecb25a11c6cff4b5356dd3fb1
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-20 16:03:51 +0300

    Update to v1.0.1

---
+++ frappe_login_session_defaults_popup/override.py
@@ -0,0 +1,11 @@
+import frappe
+
+def on_login(login_manager):
+    frappe.publish_realtime(event='login_event_triggered', message={'status': True}, after_commit=True)
+    
+
+@frappe.whitelist()
+def get_settings() -> dict:
+    return {
+        'socketio_port': frappe.conf.socketio_port,
+    }
