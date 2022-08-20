375ed41377c1acc53e50ed30e5014b360a8a261a
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-20 21:08:49 +0300

    Update to v1.0.2

--- frappe_login_session_defaults_popup/override.py
+++ frappe_login_session_defaults_popup/override.py
@@ -1,6 +1,11 @@
 import frappe
 
+
 def on_login(login_manager):
+    frappe.publish_realtime(event='login_event_triggered', message={'status': True}, after_commit=True)
+
+
+def on_session_creation(login_manager):
     frappe.publish_realtime(event='login_event_triggered', message={'status': True}, after_commit=True)
     
 
