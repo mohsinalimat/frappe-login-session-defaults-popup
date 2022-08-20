29dd1523979fb903a6ac3a83bf0730a164e4c08d
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-20 21:11:05 +0300

    Update to v1.0.2

--- frappe_login_session_defaults_popup/public/js/session_defaults_popup.bundle.js
+++ frappe_login_session_defaults_popup/public/js/session_defaults_popup.bundle.js
@@ -1,17 +1,45 @@
-async function _initLoginSessionDefaultsPopup() {
-    let res = await frappe.call({
+frappe.provide('frappe.login_session_defaults_popup');
+frappe.provide('frappe.ui.toolbar');
+
+frappe.login_session_defaults_popup.init = function() {
+    frappe.call({
         type: 'GET',
         method: 'frappe_login_session_defaults_popup.override.get_settings',
-    });
-    let data = await res.message;
-    await frappe.require([
-        'assets/frappe/js/lib/socket.io.min.js',
-        'assets/frappe/js/frappe/socketio_client.js',
-    ], () => {
-        frappe.socketio.init(data.socketio_port);
-        frappe.realtime.on('login_event_triggered', function(res) {
-            frappe.ui.toolbar.setup_session_defaults();
+    }).then(function(res) {
+        let data = res.message;
+        if (frappe.login_session_defaults_popup.on_init) {
+            frappe.login_session_defaults_popup.on_init(data);
+        }
+        frappe.require([
+            'assets/frappe/js/lib/socket.io.min.js',
+            'assets/frappe/js/frappe/socketio_client.js',
+        ], function() {
+            frappe.socketio.init(data.socketio_port);
+            if (frappe.login_session_defaults_popup.on_waiting) {
+                frappe.login_session_defaults_popup.on_waiting();
+            }
+            frappe.realtime.on('login_event_triggered', function(res) {
+                frappe.login_session_defaults_popup.on_login_triggered(res);
+            });
         });
     });
-}
-_initLoginSessionDefaultsPopup();
+};
+
+frappe.login_session_defaults_popup.on_login_triggered = function(res) {
+    if (!frappe.ui.toolbar.setup_session_defaults) {
+        frappe.timeout(1).then(function() {
+            frappe.login_session_defaults_popup.on_login_triggered(res);
+        });
+        return;
+    }
+    if (frappe.login_session_defaults_popup._is_login_triggered) return;
+    frappe.login_session_defaults_popup._is_login_triggered = true;
+    if (frappe.login_session_defaults_popup.on_login) {
+        frappe.login_session_defaults_popup.on_login(res);
+    }
+    frappe.ui.toolbar.setup_session_defaults();
+};
+
+frappe.timeout(1).then(function() {
+    frappe.login_session_defaults_popup.init();
+});
